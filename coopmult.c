09647be6b341c75389e8ca62b7b6c3540eb105df
Nikitat0 <nikitatzero@yandex.ru>
2024-07-06 19:48:42 +0700

    Revert "Reimplement coopmult on own suspend code"
    This reverts commit 6383b45cb0a3e5819b105514e562adea848aca80.

--- coopmult.c
+++ coopmult.c
@@ -2,6 +2,7 @@
 
 #include "queue.h"
 
+#include <setjmp.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
@@ -15,25 +16,24 @@ typedef struct {
 QUEUE(Tasks, tasks, Task)
 
 typedef struct {
-  char *stack, *continuation;
-} Context;
+  void* stack;
+  jmp_buf env;
+} Coroutine;
 
-QUEUE(Contexts, contexts, Context)
+QUEUE(Coroutines, coroutines, Coroutine)
 
 QUEUE(Stacks, stacks, char*)
 
 static Tasks queuedTasks;
-static Contexts suspendedTasks;
 static Stacks cachedStacks;
+static Coroutines coroutines;
 
-static char* entryContinuation;
+static jmp_buf entrypoint;
 static char* activeStack;
 
-extern void coopmult_suspend(void);
-extern void coopmult_continue(char* continuation);
-void coopmult_schedule(char* continuation);
 static void coopmult_begin_task(Task task);
 static void coopmult_end_task(void);
+static void coopmult_continue(void);
 
 void coopmult_add_task(void (*entry)(void*), void* args) {
   Task task = {.entry = entry, .args = args};
@@ -41,14 +41,18 @@ void coopmult_add_task(void (*entry)(void*), void* args) {
 }
 
 void coopmult_run(void) {
-  entryContinuation = NULL;
-  coopmult_suspend();
+  if (!setjmp(entrypoint))
+    coopmult_continue();
   while (!stacksIsEmpty(cachedStacks))
     free(stacksPop(&cachedStacks));
 }
 
 void coopmult_sleep(void) {
-  coopmult_suspend();
+  Coroutine co = {.stack = activeStack};
+  if (setjmp(co.env))
+    return;
+  coroutinesPush(&coroutines, co);
+  coopmult_continue();
 }
 
 static void coopmult_begin_task(Task task) {
@@ -57,7 +61,7 @@ static void coopmult_begin_task(Task task) {
   __asm__ volatile(
       "movq %0, %%rsp\n"
       "pushq %1\n"
-      "jmp *%3\n"
+      "jmp *%3"
       :
       : "r"(activeStack + STACK_SIZE), "r"(coopmult_end_task), "D"(task.args), "r"(task.entry));
 #elif defined(__i386__)
@@ -65,9 +69,9 @@ static void coopmult_begin_task(Task task) {
       "movl %0, %%esp\n"
       "pushl %1\n"
       "pushl %2\n"
-      "jmp *%3\n"
+      "jmp *%3"
       :
-      : "r"(activeStack + STACK_SIZE - 12), "r"(task.args), "r"(coopmult_end_task), "r"(task.entry));
+      : "r"(activeStack + STACK_SIZE), "r"(task.args), "r"(coopmult_end_task), "r"(task.entry));
 #else
   #error Unsupported platform
 #endif
@@ -75,70 +79,16 @@ static void coopmult_begin_task(Task task) {
 
 static void coopmult_end_task(void) {
   stacksPush(&cachedStacks, activeStack);
-  coopmult_schedule(NULL);
+  coopmult_continue();
 }
 
-void coopmult_schedule(char* continuation) {
-  if (entryContinuation == NULL)
-    entryContinuation = continuation;
-  else if (continuation != NULL) {
-    Context co = {.stack = activeStack, .continuation = continuation};
-    contextsPush(&suspendedTasks, co);
-  }
-
+static void coopmult_continue(void) {
   if (!tasksIsEmpty(queuedTasks))
     coopmult_begin_task(tasksPop(&queuedTasks));
-  else if (!contextsIsEmpty(suspendedTasks)) {
-    Context ctx = contextsPop(&suspendedTasks);
-    activeStack = ctx.stack;
-    coopmult_continue(ctx.continuation);
+  else if (!coroutinesIsEmpty(coroutines)) {
+    Coroutine co = coroutinesPop(&coroutines);
+    activeStack = co.stack;
+    longjmp(co.env, true);
   } else
-    coopmult_continue(entryContinuation);
+    longjmp(entrypoint, true);
 }
-
-#if defined(__x86_64__)
-__asm__(
-    "coopmult_suspend:\n"
-    "pushq %rbp\n"
-    "pushq %rbx\n"
-    "pushq %r12\n"
-    "pushq %r13\n"
-    "pushq %r14\n"
-    "pushq %r15\n"
-    "sub $8, %rsp\n"
-    "lea -8(%rsp), %rdi\n"
-    "call coopmult_schedule\n"
-    "add $8, %rsp\n"
-    "popq %r15\n"
-    "popq %r14\n"
-    "popq %r13\n"
-    "popq %r12\n"
-    "popq %rbx\n"
-    "popq %rbp\n"
-    "ret\n"
-    "coopmult_continue:\n"
-    "mov %rdi, %rsp\n"
-    "ret\n");
-#elif defined(__i386__)
-__asm__(
-    "coopmult_suspend:\n"
-    "pushl %ebp\n"
-    "pushl %ebx\n"
-    "pushl %esi\n"
-    "pushl %edi\n"
-    "subl $8, %esp\n"
-    "leal -8(%esp), %eax\n"
-    "pushl %eax\n"
-    "call coopmult_schedule\n"
-    "add $12, %esp\n"
-    "popl %edi\n"
-    "popl %esi\n"
-    "popl %ebx\n"
-    "popl %ebp\n"
-    "ret\n"
-    "coopmult_continue:\n"
-    "movl 4(%esp), %esp\n"
-    "ret\n");
-#else
-  #error Unsupported platform
-#endif
